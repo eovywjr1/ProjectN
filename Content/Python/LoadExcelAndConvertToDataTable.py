7be3ec12d2903f7ad5d4dcc77f6d63880888c427
eovywjr1 <40791869+eovywjr1@users.noreply.github.com>
2024-11-07 02:20:19 +0900

    리팩토링 및 에러 메시지 추가

--- Content/Python/LoadExcelAndConvertToDataTable.py
+++ Content/Python/LoadExcelAndConvertToDataTable.py
@@ -3,188 +3,167 @@ import pandas as pd
 import os
 from typing import Dict, List, Tuple
 
-excelPath = unreal.SystemLibrary.get_project_directory() + "Data/DataExcel"
-csvPath = unreal.SystemLibrary.get_project_directory() + "Data/DataCSV"
+ProjectDir = unreal.SystemLibrary.get_project_directory()
+ExcelDir = ProjectDir + "Data/DataExcel"
+CSVDir = ProjectDir + "Data/DataCSV"
 
-findPropertyColumnIndex = 0
+PropertyRowIdentifier = "Property"
+PropertyColumnIndex = 0
 
-class UnrealExcelConverter:
-    def __init__(self):
-        self.editor_asset_lib = unreal.EditorAssetLibrary()
-        
-        self.type_mapping = {
+UnrealTypeMapping = {
             'int': 'int32',
             'string': 'FString',
             'list:int': 'TArray<int32>',
             'list:string': 'TArray<FString>'
-        }
+}
 
-    def loadExcel(self, xlsx):
-        file_path = excelPath + "/" + xlsx
-         
+UnrealEditorAssetLib = unreal.EditorAssetLibrary()
+
+def ProcessConvertExcel() -> bool:
+    FileList = os.listdir(ExcelDir)
+    ExcelFileList = []
+
+    for File in FileList:
+        if File.endswith(".xlsx"):
+            ExcelFileList.append(File)
+
+    LodeadExcelAndWorkSheetNameList = []
+    
+    for ExcelFileNameAndExtension in ExcelFileList:
+        if(ExcelFileNameAndExtension.startswith("~")):
+            continue
+
+        unreal.log_error(f"{ExcelFileNameAndExtension} Convert Start")
+
         try:
-            allSheet = pd.read_excel(file_path, sheet_name=None)
+            ExcelFilePath = ExcelDir + "/" + ExcelFileNameAndExtension
+            LoadedExcel = pd.read_excel(ExcelFilePath, sheet_name=None)
+
         except Exception as e:
-            unreal.log_error(f"엑셀 파일 읽기 실패: {str(e)}")
-            return
-        
-        for sheetName, excelData in allSheet.items():
-            if excelData.empty:
+            unreal.log_error(f"엑셀 파일({ExcelFileNameAndExtension}) 읽기 실패: {str(e)}")
+            return False
+
+        for SheetName, SheetData in LoadedExcel.items():
+            if SheetData.empty:
                 continue
 
-            unreal.log_error(f"{sheetName} 워크시트 Convert Start")
+            # SheetName 중복 체크
+            for LoadedFileName, LoadedSheetName in LodeadExcelAndWorkSheetNameList:
+                if LoadedSheetName == SheetName:
+                    unreal.log_error(f"{LoadedFileName} - {LoadedSheetName} 과 {ExcelFileNameAndExtension} - {SheetName}이 중복됩니다. 워크시트 이름을 수정해주세요")
+                    return False
 
-            columnTypes = {}
+            LodeadExcelAndWorkSheetNameList.append((ExcelFileNameAndExtension, SheetName))
 
-            try:
-                first_column = excelData.iloc[:, 0].tolist()
-
-                propertyMask = excelData.iloc[:, 0] == "Property"
-
-                if not propertyMask.any():
-                    unreal.log_error(f"{xlsx} 파일의 {sheetName} 워크시트에 {findPropertyColumnIndex} 행에 Property 값이 없습니다. 수정해주세요")
-                    return
-
-                propertyRows = excelData[propertyMask].index[0]
-
-                propertyData = excelData.iloc[propertyRows, 1:]
-
-                typeData = excelData.iloc[propertyRows - 1, 1:].values
-
-                for property, type in zip(propertyData, typeData):
-                    ueType = self.type_mapping.get(str(type))
-                    if property.startswith("_") == False and ueType is None:
-                        unreal.log_error(f"{xlsx} 파일에 Type: {type} 은 없습니다. int, string, list:int, list:string만 입력해주세요.")
-                        return
-
-                    columnTypes[property] = ueType
-
-                self.convertExcelToCSV(xlsx, sheetName, excelData, columnTypes)
-
-            except Exception as e:
-                unreal.log_error(f"처리 중 에러 발생: {str(e)}")
-
-    def convertExcelToCSV(self, xlsx, sheetName, excelData: pd.DataFrame, columnTypes: Dict[str, str]):
-        csvFilePath = f"{csvPath}/{sheetName}.csv"
-        if os.path.exists(csvFilePath):
-           try:
-               os.remove(csvFilePath)
-           except Exception as e:
-               unreal.log_error(f"Failed to delete CSV file: {str(e)}")
-               return
-   
-        properties = []
-        exceptPropertyRowIndex = []
-
-        for columnTypeIndex, (key, value) in enumerate(columnTypes.items()):
-            if key.startswith("_"):
-                exceptPropertyRowIndex.append(columnTypeIndex + 1)
-            else:
-                properties.append(key)
-        
-        csvData = []
-        csvData.append(','.join(properties))
-
-        propertyColumn = excelData.columns[findPropertyColumnIndex]
-        propertyMask = excelData[propertyColumn].astype(str).eq('Property')
-        if not propertyMask.values.any():
-            return
-            
-        propertyRowIndex = excelData[propertyMask].index[0]
-        startRowIndex = propertyRowIndex + 1
-        startColIndex = findPropertyColumnIndex + 1
-
-        for rowIndex in range(startRowIndex, len(excelData)):
-            rowData = []
-                
-            for colIndex in range(startColIndex, len(excelData.columns)):
-                if colIndex in exceptPropertyRowIndex:
-                    continue
-
-                value = excelData.iloc[rowIndex, colIndex]
-                if pd.isna(value):
-                    continue
-                
-                colType = columnTypes.get(excelData.iloc[propertyRowIndex, colIndex])
-                
-                if 'TArray' in colType:
-                    if ';' in str(value):
-                        values = str(value).split(';')
-                        if 'int32' in colType:
-                            value = f"({','.join(values)})"
-                        else:
-                            values = [f'""{v}""' for v in values]
-                            value = f"({','.join(values)})"
-
-                        value = f'"{value}"'
-
-                rowData.append(str(value))
-            
-            csvData.append(','.join(rowData))
-
-        
-        with open(csvFilePath, 'w', encoding='utf-8') as f:
-            f.write('\n'.join(csvData))
-
-        self.createDatatable(xlsx, sheetName)
-
-    def createDatatable(self, xlsx: str, sheetName):
-        try:
-            datatablePath = f"/Game/ProjectN/DataTable/DT_{sheetName}"
-            
-            if self.editor_asset_lib.does_asset_exist(datatablePath):
-                self.editor_asset_lib.delete_asset(datatablePath)
-
-            headerPath = f"/Script/ProjectN.{sheetName}"
-            loadedStruct = unreal.load_object(None, headerPath)
-            if not loadedStruct:
-                return
-            
-            datatableFactory = unreal.CSVImportFactory()
-            datatableFactory.automated_import_settings.import_row_struct = loadedStruct
-            
-            task = unreal.AssetImportTask()
-            task.filename = f"{csvPath}/{sheetName}.csv"
-            task.destination_path = "/Game/ProjectN/DataTable"
-            task.destination_name = f"DT_{sheetName}"
-            task.automated = True
-            task.save = True
-            task.factory = datatableFactory
-
-            unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([task])
-            
-        except Exception as e:
-            unreal.log_error(f"Fatal error creating DataTable: {str(e)}")
-            unreal.log_error(f"Error type: {type(e).__name__}")
-
-    def convertExcelToDatatable(self) -> bool:
-        fileList = os.listdir(excelPath)
-        xlsxFileList = []
-
-        for file in fileList:
-            if file.endswith(".xlsx"):
-                xlsxFileList.append(file)
-        
-        for xlsx in xlsxFileList:
-            if(xlsx.startswith("~")):
-                continue
-
-            unreal.log_error(f"{xlsx} Convert Start")
+            unreal.log_error(f"{SheetName} 워크시트 Convert Start")
 
             try:
-                self.loadExcel(xlsx)
-            
+                ConvertExcelToCSV(ExcelFileNameAndExtension, SheetName, SheetData)
+
+                ConvertCSVToDataTable(SheetName)
+
             except Exception as e:
-                unreal.log_error(f"Conversion failed: {str(e)}")
+                unreal.log_error(f"처리 중 에러 발생 {ExcelFileNameAndExtension}: {str(e)}")
                 return False
+        
+        unreal.log_error(f"{ExcelFileNameAndExtension} Convert End")
+
+def ConvertExcelToCSV(ExcelFileNameAndExtension, SheetName, SheetData: pd.DataFrame):
+    PropertyMask = SheetData.iloc[:, 0] == PropertyRowIdentifier
+    if not PropertyMask.any():
+        unreal.log_error(f"{ExcelFileNameAndExtension} 파일의 {SheetName} 워크시트에 {PropertyColumnIndex} 행에 Property 값이 없습니다. 수정해주세요")
+        return
+
+    PropertyRowIndex = SheetData[PropertyMask].index[0]
+    TypeRowIndex = PropertyRowIndex - 1
+    PropertyRowDataList = SheetData.iloc[PropertyRowIndex, 1:]
+
+    LoadPropertyList = []
+    ExceptPropertyColumnIndexList = []
+
+    for ColumnIndex in range(len(PropertyRowDataList)):
+        if PropertyRowDataList[ColumnIndex].startswith("_"):
+            ExceptPropertyColumnIndexList.append(ColumnIndex + 1)
+        else:
+            LoadPropertyList.append(PropertyRowDataList[ColumnIndex])
+    
+    CSVData = []
+    CSVData.append(','.join(LoadPropertyList))
+        
+    StartRowIndex = PropertyRowIndex + 1
+    StartColIndex = PropertyColumnIndex + 1
+
+    for RowIndex in range(StartRowIndex, len(SheetData)):
+        RowData = []
             
-            unreal.log_error(f"{xlsx} Convert End")
+        for ColIndex in range(StartColIndex, len(SheetData.columns)):
+            if ColIndex in ExceptPropertyColumnIndexList:
+                continue
+
+            Value = SheetData.iloc[RowIndex, ColIndex]
+            if pd.isna(Value):
+                continue
             
-        return True
+            Type = UnrealTypeMapping.get(str(SheetData.iloc[TypeRowIndex, ColIndex]))
+            
+            if 'TArray' in Type and ';' in str(Value):
+                TempValue = str(Value).split(';')
+
+                if 'int32' in Type:
+                    Value = f"({','.join(TempValue)})"
+                elif 'FString' in Type:
+                    TempValue = [f'""{v}""' for v in TempValue]
+                    Value = f"({','.join(TempValue)})"
+
+                Value = f'"{Value}"'
+
+            RowData.append(str(Value))
+        
+        CSVData.append(','.join(RowData))
+
+    CSVFilePath = f"{CSVDir}/{SheetName}.csv"
+    if os.path.exists(CSVFilePath):
+       try:
+           os.remove(CSVFilePath)
+       except Exception as e:
+           unreal.log_error(f"Failed to delete CSV file: {str(e)}")
+           return
+    
+    with open(CSVFilePath, 'w', encoding='utf-8') as f:
+        f.write('\n'.join(CSVData))
+
+def ConvertCSVToDataTable(SheetName):
+    try:
+        DataTableDir = "/Game/ProjectN/DataTable"
+        DataTablePath = DataTableDir + f"/DT_{SheetName}"
+        
+        if UnrealEditorAssetLib.does_asset_exist(DataTablePath):
+            UnrealEditorAssetLib.delete_asset(DataTablePath)
+
+        DataTableStructHeaderPath = f"/Script/ProjectN.{SheetName}"
+        DataTableStructObject = unreal.load_object(None, DataTableStructHeaderPath)
+        if not DataTableStructObject:
+            unreal.log_error(f"해당 데이터 테이블 구조체가 없습니다. 클라에게 요청해주세요 {DataTableStructHeaderPath}")
+            return
+        
+        DatatableFactory = unreal.CSVImportFactory()
+        DatatableFactory.automated_import_settings.import_row_struct = DataTableStructObject
+        
+        DataTableTask = unreal.AssetImportTask()
+        DataTableTask.filename = f"{CSVDir}/{SheetName}.csv"
+        DataTableTask.destination_path = DataTableDir
+        DataTableTask.destination_name = f"DT_{SheetName}"
+        DataTableTask.automated = True
+        DataTableTask.save = True
+        DataTableTask.factory = DatatableFactory
+        unreal.AssetToolsHelpers.get_asset_tools().import_asset_tasks([DataTableTask])
+        
+    except Exception as e:
+        unreal.log_error(f"Fatal error creating DataTable: {str(e)}")
+        unreal.log_error(f"Error type: {type(e).__name__}")
 
 def main():
-    converter = UnrealExcelConverter()
-    success = converter.convertExcelToDatatable()
+    success = ProcessConvertExcel()
     
     if success:
         print("Conversion completed successfully")

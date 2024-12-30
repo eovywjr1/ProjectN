e9c86444c13fab18e75ea041ca10a37f81eb8fe7
eovywjr1 <eovywjr1@naver.com>
2024-12-31 05:46:33 +0900

    bat 파일 에디터 실행 전에 generate visual stduio project file 먼저 하도록 변경

--- UnrealEditor-DebugGame.bat
+++ UnrealEditor-DebugGame.bat
@@ -2,5 +2,7 @@
 set UE_PATH=E:\Program Files\Epic Games\UE_5.1
 set PROJECT_PATH=E:\Unreal\ProjectN
 
+"%UE_PATH%\Engine\Binaries\Win64\UnrealVersionSelector.exe" /projectfiles "%PROJECT_PATH%\ProjectN.uproject"
 "%UE_PATH%\Engine\Binaries\Win64\UnrealEditor-Win64-DebugGame.exe" "%PROJECT_PATH%\ProjectN.uproject"
+
 pause

f2abdf3984034fc3befd1723b3cd0719beaf3712
eovywjr1 <eovywjr1@naver.com>
2024-12-31 02:06:44 +0900

    DebugGame으로 에디터 실행하는 bat 파일 추가

---
+++ UnrealEditor-DebugGame.bat
@@ -0,0 +1,6 @@
+@echo off
+set UE_PATH=E:\Program Files\Epic Games\UE_5.1
+set PROJECT_PATH=E:\Unreal\ProjectN
+
+"%UE_PATH%\Engine\Binaries\Win64\UnrealEditor-Win64-DebugGame.exe" "%PROJECT_PATH%\ProjectN.uproject"
+pause
